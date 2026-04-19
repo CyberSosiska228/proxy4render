59a050095c25aae7c817599d6164a99c605e7e85
CyberSosiska_228 <kotyshevtim@gmail.com>
2026-04-19 21:39:50 +0000

    Meow

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import asyncio
-from asyncio import StreamReader, StreamWriter
+import os
 
 async def handle_client(reader, writer):
     writer.write(b"Meow\r\n")
@@ -7,9 +7,11 @@ async def handle_client(reader, writer):
     writer.close()
     await writer.wait_closed()
 
-
 async def main():
-    server = await asyncio.start_server(handle_client, '0.0.0.0', 80)
+    port = int(os.environ.get("PORT", 8080))
+    server = await asyncio.start_server(handle_client, '0.0.0.0', port)
+    print(f"Server is listening on port {port}")
     await server.serve_forever()
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
